af9ab478441725fc6ad8732732bcdecb927d5446
BCL <bcl.contract.tester@gmail.com>
2021-02-09 20:57:45 -0800

    Added Zak's in-class js demo files

---
+++ Lecture 2/Zak's in-class files/Zaks_completed_inclass_lecture_script.js
@@ -0,0 +1,44 @@
+let strNum1 = prompt('Guess a number between 1 and 10');
+let strNum2 = 10;
+strNum2--;
+alert(SstrNum2);
+
+// let message = "Welcome to the JavaScript course.";
+// let inInstructor = true;
+// let yearsTeaching = 20;
+// let zaksAge = null;
+// let courseName;
+// console.log(`Zak Ruvalcaba is the instructor. ${message}`);
+
+// let car = new Object();
+// car.color = "red";
+// car.manufacturer = "Chevy";
+// car.model = "Camaro";
+// console.log(`The ${car.color} ${car.manufacturer} ${car.model} just drove off!`);
+// console.log("The " + car.color + " " + car.manufacturer + " " + car.model + " just drove off!");
+
+// Example of primitive data types
+// let strNum1 = 'Hello World'; // string primitive
+// let strNum2 = 10; // numeric primitive
+// let strNum3 = false; // Boolean primitive
+// let strNum4 = null; // object primitive
+// let strNum5; // undefined
+
+// console.log('Num 1 ' + typeof strNum1);
+// console.log('Num 2 ' + typeof strNum2);
+// console.log('Num 3 ' + typeof strNum3);
+// console.log('Num 4 ' + typeof strNum4);
+// console.log('Num 5 ' + typeof strNum5);
+
+// // Display result within an alert box
+// alert('You guessed the number ' + strNum);
+
+// // Display result within console window
+// console.log('You guessed the number ' + strNum);
+// console.info('You guessed the number ' + strNum);
+// console.warn('You guessed the number ' + strNum);
+// console.error('You guessed the number ' + strNum);
+
+// // Display result within web page
+// document.write('You guessed the number ' + strNum);
+
---
+++ Lecture 2/Zak's in-class files/Zaks_completed_lab3_script.js
@@ -0,0 +1,4 @@
+let milesDriven = prompt('How many miles did you drive?');
+let gallons = prompt('How many gallons of fuel does your car hold?');
+let milesPerGallon = milesDriven / gallons;
+document.write(`Your car gets ${milesPerGallon.toFixed(2)} miles per gallon.`);
